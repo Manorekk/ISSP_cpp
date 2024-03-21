4d1de670eb80b86939062065d446102c82e36b39
Zbigniew Koza <zbigniew.koza@uwr.edu.pl>
2024-03-21 10:32:05 +0100

    drobne poprawki do listy 1

--- 01/zadanie1.cpp
+++ 01/zadanie1.cpp
@@ -8,19 +8,25 @@ int main()
     double A = 0;
     double B = 1;
     double C = 0;
-    for(int i = 1; i <= N; i++)
+    for (int i = 1; i <= N; i++)
     {
-        A += (pow(-1, i + 1))/(2 * i - 1);
-        B *= (4 * pow(i,2))/((4 * pow(i,2)) - 1);
-        C += 1/pow(((2 * i) - 1),2);
+        A += (pow(-1, i + 1)) / (2.0 * i - 1.0);
+        B *= (4.0 * i * i) / ((4.0 * i * i) - 1.0);
+        C += 1 / pow((2.0 * i - 1.0), 2);
     }
     double wynikA = 4 * A;
     double wynikB = 2 * B;
-    double wynikC = sqrt(8 * C);
+    double wynikC = sqrt(8.0 * C);
 
-    std::cout <<"a) pi: "<< M_PI << "\t" << "wyliczone: " <<wynikA<<"\t"<< "roznica: "<< M_PI - wynikA << std::endl;
-    std::cout <<"b) pi: "<< M_PI << "\t" << "wyliczone: " <<wynikB<<"\t"<< "roznica: "<< M_PI - wynikB << std::endl;
-    std::cout <<"c) pi: "<< M_PI << "\t" << "wyliczone: " <<wynikC<<"\t"<< "roznica: "<< M_PI - wynikC << std::endl;
+    std::cout << "a) pi: " << M_PI << "\t"
+              << "wyliczone: " << wynikA << "\t"
+              << "roznica: " << M_PI - wynikA << std::endl;
+    std::cout << "b) pi: " << M_PI << "\t"
+              << "wyliczone: " << wynikB << "\t"
+              << "roznica: " << M_PI - wynikB << std::endl;
+    std::cout << "c) pi: " << M_PI << "\t"
+              << "wyliczone: " << wynikC << "\t"
+              << "roznica: " << M_PI - wynikC << std::endl;
 
     return 0;
 }
